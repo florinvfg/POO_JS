8a39583abd8a1ebe07a906c441ec7bfe3f15ce90
Florin Giany Virlan <126181506+florinvfg@users.noreply.github.com>
2024-06-10 18:21:01 +0200

    Add files via upload

--- POO_JS/src/main/webapp/js/Vehiculo.js
+++ POO_JS/src/main/webapp/js/Vehiculo.js
@@ -1,14 +1,33 @@
-class Vehiculo{
-   marca;
+// Definición de la clase Vehiculo
+class Vehiculo {
+    // Declaración de las propiedades de la clase
+    marca;
     modelo;
     color;
     potencia;
 
-    constructor (marca,modelo,color,potencia) {
-
-
-
+    // Constructor que inicializa las propiedades de un objeto Vehiculo
+    constructor(marca, modelo, color, potencia) {
+        // Asignación de los parámetros a las propiedades de la clase
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color; 
+        this.potencia = potencia;
     }
 
-
+    // Método para mostrar los detalles del vehículo
+    mostrar() {
+        // Imprime en consola los detalles del vehículo utilizando template literals
+        console.log(`Coche Marca ${this.marca} con modelo ${this.modelo} y es de color ${this.color} con la potencia ${this.potencia}`);
+    }
 }
+
+// Creación de una nueva instancia de la clase Vehiculo
+let miCoche = new Vehiculo("Ford", "Kuga", "Gris", "110CV");
+// Creación de otra instancia de la clase Vehiculo
+let miCoche2 = new Vehiculo("Opel", "Vectra", "Rojo", "13CV");
+
+// Llamada al método mostrar para la primera instancia
+miCoche.mostrar();
+// Llamada al método mostrar para la segunda instancia
+miCoche2.mostrar();
